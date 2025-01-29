301fe37010d1b437c727f674398d2713ec513a9a
devavratwadekar <devavrat.wadekar@gmail.com>
2025-01-29 22:58:33 +0530

    Operation on data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -46,3 +46,39 @@ console.log(valueInBoolean9);            // "" -> false; "devavrat" ->true
 console.log(typeof valueInBoolean9);     // boolean
 
 
+// ******************************* Operations **********************************
+console.log(2+2);   // 4
+console.log(2-2);   // 0
+console.log(2*2);   // 4
+console.log(2**3);  // 8
+console.log(2/3);   // 0.666666666666
+console.log(2%3);   // 2
+
+let str1 = "Hello";
+let str2 = " Devavrat";
+let str3 = str1 + str2;
+console.log(str3);       // Hello Devavrat
+
+
+console.log(1 + "2");         // 12
+console.log("1" + 2);         // 12
+console.log("1" + 2 + 2);     // 122
+console.log(+true);           // 1
+console.log(+"");             // 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 3;
+console.log(num1 + num2 + num3);    // 15
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);           // 101
+
+
+
+
+
+
+
+
+
