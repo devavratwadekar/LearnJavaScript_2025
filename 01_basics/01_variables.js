efeb363ed9bbb991f40c7f45a1ab25bd46903261
Devavrat Kishor Wadekar <83110314+devavratwadekar@users.noreply.github.com>
2025-04-04 18:38:09 +0000

    variables commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const roll_number = 1001;
+
+let full_name = "Devavrat Kishor Wadekar";
+
+var gender = "Male";
+
+let email_address = "devavrat@google.com";
+
+let age = 25;
+
+console.table([roll_number, full_name,  gender, email_address, age]);
