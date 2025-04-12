3600c2ff83df5fa69be05060820cdb49455770f7
devavratwadekar <devavrat.wadekar@gmail.com>
2025-04-12 18:32:41 +0530

    comment update

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,7 +2,7 @@
 
 // alert(3+3);  // we are using nodejs, not browser.
 
-console.log(3 + 3); // code readability shoul be HIGH.
+console.log(3 + 3); // code readability should be HIGH.
 
 console.log("Devavrat");
 
