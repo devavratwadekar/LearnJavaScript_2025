8eff402981befe555df7aac813f2f07555415f8a
devavratwadekar <devavrat.wadekar@gmail.com>
2025-04-12 19:39:41 +0530

    Postfix & Prefix increment commit

---
+++ 01_basics/03_prefix_postfix.js
@@ -0,0 +1,12 @@
+// Postfix increment
+let x = 3;
+const y = x++;
+console.log("y : " + y)     // 3
+console.log("x : " + x)     // 4
+
+
+// Prefix increment
+let a = 10;
+const b = ++a;
+console.log("a : " + a)     // 3
+console.log("b : " + b)     // 4
